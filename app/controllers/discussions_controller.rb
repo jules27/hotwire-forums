d6a0ddefbdd38ca05d9ef75e9462640da98b2e2c
Julie Mao <jmao@wrapbook.com>
2022-05-16 11:00:17 -0700

    Follow the tutorial up until the inline editing part.

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -1,7 +1,55 @@
 class DiscussionsController < ApplicationController
 	before_action :authenticate_user!
+  before_action :set_discussion, only: [:show, :edit, :update, :destroy]
+
+  def show
+  end
 
 	def index
 		@discussions = Discussion.all
 	end
+
+  def new
+    @discussion = Discussion.new
+  end
+
+  def create
+    @discussion = Discussion.new(discussion_params)
+
+    respond_to do |format|
+      if @discussion.save
+        format.html { redirect_to @discussion, notice: "Discussion created" }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    respond_to do |format|
+      if @discussion.update(discussion_params)
+        format.html { redirect_to @discussion, notice: "Discussion updated" }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @discussion.destroy!
+    redirect_to discussion_path, notice: 'Discussion removed'
+  end
+
+  private
+
+  def discussion_params
+    params.require(:discussion).permit(:name, :closed, :pinned)
+  end
+
+  def set_discussion
+    @discussion = Discussion.find(params[:id])
+  end
 end
